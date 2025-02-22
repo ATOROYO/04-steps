fc11117a2bd3d472793f5a4437d6bd20c12b9610
ATOROYO <davidatoroyosika@gmail.com>
2025-02-22 23:08:27 +0300

    Display current step message in the App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const messages = [
 ];
 
 export default function App() {
+  const step = 1;
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -13,7 +15,9 @@ export default function App() {
         <div>3</div>
       </div>
 
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
 
       <div className="buttons">
         <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
