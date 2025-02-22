fd2e7c0da3c9b6b2b8c03ab0205c660f6f1ad774
ATOROYO <davidatoroyosika@gmail.com>
2025-02-22 22:55:21 +0300

    Update Previous button styling in App component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ export default function App() {
       <p className="message">Hello</p>
 
       <div className="buttons">
-        <button>Previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
         <button>Next</button>
       </div>
     </div>
